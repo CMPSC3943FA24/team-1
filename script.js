2a164e98b671d92515f1b0367ef90c331fb1ac39
William Lint <wilrlin2@email.ecok.edu>
2024-10-22 21:53:59 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  // Initialize the map
+  map = new Map(document.getElementById("map"), {
+    center: { lat: 34.644, lng: -97.161 },
+    zoom: 15,
+  });
+
+  // Create the KML layer
+  const kmlLayer = new google.maps.KmlLayer({
+    url: "https://raw.githubusercontent.com/DarthLiam93/Test-area/refs/heads/test-branch/WynnewoodPrimaryLines.kml",
+    suppressInfoWindows: false,
+    preserveViewport: false,
+    map: map
+  });
+}
+
+initMap();
